cdf772c43b0209fb34b6d14a257a7d01ba5f4ee2
lunafalya <lunafalyais@gmail.com>
2025-10-04 21:31:13 +0700

    push

---
+++ resources/views/home/landing.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lashie Lust</title>
+     <link rel="icon" href="img/core-img/logo.png">
+</head>
+<body>
+    <div class="preloader d-flex align-items-center justify-content-center">
+        <div class="preloader-circle"></div>
+        <div class="preloader-img">
+            <img src="img/core-img/logo.png" alt="">
+        </div>
+    </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\AdminController;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home.landing');
+})->name('landing');
+
+// Route::get('/dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
